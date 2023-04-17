396b716840b176f9278b15e119d376d93102b250
Emman <emmanuel.lamothe@gmail.com>
2023-04-16 22:16:48 -0400

    test

--- srcs/main.c
+++ srcs/main.c
@@ -6,46 +6,103 @@
 /*   By: emman <emman@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 10:27:10 by emlamoth          #+#    #+#             */
-/*   Updated: 2023/04/16 19:23:26 by emman            ###   ########.fr       */
+/*   Updated: 2023/04/16 22:10:30 by emman            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pushswap.h"
 
-void	ft_sort_3(t_data *data)
+void	ft_sort_3(t_data *data, char set)
 {
-	if(data->stack_a->index > data->stack_a->next->index 
-			&& data->stack_a->next->index > data->stack_a->next->next->index)
+	t_stack *temp;
+
+	if(set == 'a')
+		temp = data->stack_a;
+	else
+		temp = data->stack_b;
+	
+	if(temp->index > temp->next->index 
+			&& temp->next->index > temp->next->next->index)
 		{
-			ft_rot_kind(data, 'a');
-			ft_swap_kind(data, 'a');
+			ft_rot_kind(data, set);
+			ft_swap_kind(data, set);
 		}
-	else if(data->stack_a->index > data->stack_a->next->index 
-			&& data->stack_a->index > data->stack_a->next->next->index)
-		ft_rot_kind(data, 'a');
-	else if(data->stack_a->index > data->stack_a->next->index)
-		ft_swap_kind(data, 'a');
-	else if (data->stack_a->index < data->stack_a->next->index 
-			&& data->stack_a->index > data->stack_a->next->next->index)
-		ft_rrot_kind(data, 'a');
-	else if (data->stack_a->index < data->stack_a->next->index 
-			&& data->stack_a->index < data->stack_a->next->next->index)
+	else if(temp->index > temp->next->index 
+			&& temp->index > temp->next->next->index)
+		ft_rot_kind(data, set);
+	else if(temp->index > temp->next->index)
+		ft_swap_kind(data, set);
+	else if (temp->index < temp->next->index 
+			&& temp->index > temp->next->next->index)
+		ft_rrot_kind(data, set);
+	else if (temp->index < temp->next->index 
+			&& temp->index < temp->next->next->index)
 	{
-		ft_swap_kind(data, 'a');
-		ft_rot_kind(data, 'a');
+		ft_swap_kind(data, set);
+		ft_rot_kind(data, set);
 	}
+}
+
+int	ft_find_biggest(t_data *data, char set)
+{
+	int biggest;
+	t_stack *temp;
+
+	biggest = INT_MIN;
+	if(set == 'a')
+		temp = data->stack_a;
+	else
+		temp = data->stack_b;
+	while(temp != NULL)
+	{
+		if(biggest < temp->index)
+			biggest = temp->index;
+		temp = temp->next;
+	}
+	return (biggest);
+}
+
+int ft_find_smallest(t_data *data, char set)
+{
+	int smallest;
+	t_stack *temp;
+
+	smallest = INT_MAX;
+	if(set == 'a')
+		temp = data->stack_a;
+	else
+		temp = data->stack_b;
+	while(temp != NULL)
+	{
+		if(smallest > temp->index)
+			smallest = temp->index;
+		temp = temp->next;
+	}
+	return (smallest);
 }
 
 void	ft_sort_5(t_data *data)
 {
+	int small;
+	int big;
+	
+	big = ft_find_biggest(data, 'a');
+	small = ft_find_smallest(data, 'a');
 	while(ft_stacksize(data->stack_a) > 3)
-		ft_push_b(data);
-	ft_sort_3(data);
+	{
+		if(data->stack_a->index == small || data->stack_a->index == big)
+			ft_push_b(data);
+		ft_rot_kind(data, 'a');
+	}
+	if(ft_sorted(data->stack_a) == FALSE && ft_stacksize(data->stack_a) == 3)
+		ft_sort_3(data, 'a');
 	if(data->stack_b->index < data->stack_b->next->index)
 		ft_swap_kind(data, 'b');
-	
-
+	ft_push_a(data);
+	ft_rot_kind(data, 'a');
+	ft_push_a(data);
 }
+
 int main(int argc, char **argv)
 {
 	t_data *data;
@@ -64,7 +121,10 @@ int main(int argc, char **argv)
 			ft_printlist(data->stack_b);
 			ft_printf("\n");
 			
-			ft_sort_3(data);
+			if(ft_stacksize(data->stack_a) == 3)
+				ft_sort_3(data, 'a');
+			else if (ft_stacksize(data->stack_a) == 5)
+				ft_sort_5(data);
 			
 			ft_printf("LIST A\n");
 			ft_printlist(data->stack_a);
@@ -73,11 +133,13 @@ int main(int argc, char **argv)
 			ft_printlist(data->stack_b);
 			ft_printf("\n");
 
+			ft_printf("%d\n", ft_find_biggest(data, 'a'));
+			ft_printf("%d\n", ft_find_smallest(data, 'a'));
+
 		}
 		else
 		{
 			ft_pushswap_free(1);
-			
 		}
 		ft_pushswap_free(0);
 	}
